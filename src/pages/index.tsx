91e10019fde7fd1325e92161676048ec39db28d9
Hunchojoe19 <enubiakjoseph@gmail.com>
2024-04-29 13:55:16 +0100

    ..

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import { UseSelector, useDispatch, useSelector } from "react-redux";
-const inter = Inter({subsets: ["latin"] });
-import { decrement, increment, selectValue } from "../redux/slices/counterSlice";
-import type { RootState } from "../redux/store/store";
-
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   // const count = useSelector(selectValue)
